d1bf9b5d72450308c3736b66ab49c8b8ae94c4ff
Serares <jianu.rares30@gmail.com>
2020-10-24 22:21:12 +0300

    chore: Add gh-pages branch

---
+++ precache-manifest.a2e6bfd835e265a8dbdc87e1dbbeed26.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "563678b88e7fd45a2570",
+    "url": "/redesigned-robot/static/css/4.0d2fe87c.chunk.css"
+  },
+  {
+    "revision": "607eab1c6346eaf29b29",
+    "url": "/redesigned-robot/static/css/main.c1d50051.chunk.css"
+  },
+  {
+    "revision": "b604ec801ca21e97ea41",
+    "url": "/redesigned-robot/static/js/runtime~main.438712f7.js"
+  },
+  {
+    "revision": "9b1e7688e918e576818a",
+    "url": "/redesigned-robot/static/js/2.e72fb28e.chunk.js"
+  },
+  {
+    "revision": "fbf9e67388d5a81440760f4672f74ec3",
+    "url": "/redesigned-robot/static/media/burger-logo.fbf9e673.png"
+  },
+  {
+    "revision": "33f8407c36bd2b2abdde",
+    "url": "/redesigned-robot/static/js/3.ee9be1ea.chunk.js"
+  },
+  {
+    "revision": "607eab1c6346eaf29b29",
+    "url": "/redesigned-robot/static/js/main.aa43b982.chunk.js"
+  },
+  {
+    "revision": "563678b88e7fd45a2570",
+    "url": "/redesigned-robot/static/js/4.c8542e6a.chunk.js"
+  },
+  {
+    "revision": "8b946406977ca389e8d5",
+    "url": "/redesigned-robot/static/js/5.4a2f0fae.chunk.js"
+  },
+  {
+    "revision": "8b946406977ca389e8d5",
+    "url": "/redesigned-robot/static/css/5.ddb7eeaf.chunk.css"
+  },
+  {
+    "revision": "33f8407c36bd2b2abdde",
+    "url": "/redesigned-robot/static/css/3.6b6cd3c1.chunk.css"
+  },
+  {
+    "revision": "473f1c9a7ab33665bf9e04d6ee6c7b62",
+    "url": "/redesigned-robot/index.html"
+  }
+];
